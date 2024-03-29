f153fafa28782bcce1c4e4b081947e72582c8a1a
Wes Sackenheim <wjsacken@gmail.com>
2024-03-29 08:09:59 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import sal from 'sal.js';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { BackToTopComponent } from './shared/components/back-to-top/back-to-top.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet,BackToTopComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+
+export class AppComponent {
+  title = 'Wallace & Graham';
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+
+
+}
