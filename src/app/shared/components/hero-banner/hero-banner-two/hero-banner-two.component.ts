b0b251c04f1f71cdeaf06736ec8c3d29af4f5412
wjsacken <wsackenheim@artscube.biz>
2024-04-24 16:44:58 -0400

    bg test

--- src/app/shared/components/hero-banner/hero-banner-two/hero-banner-two.component.ts
+++ src/app/shared/components/hero-banner/hero-banner-two/hero-banner-two.component.ts
@@ -12,5 +12,6 @@ import { NgOptimizedImage } from '@angular/common'; // Import NgOptimizedImage
 })
 export class HeroBannerTwoComponent {
   imageUrl: string = 'https://imagedelivery.net/N7RaiyPSCRCLRce1-vDkeg/be5b6665-7d00-4a2c-c1b4-eb4850229300/partners';
+  imageBg: string = 'https://imagedelivery.net/N7RaiyPSCRCLRce1-vDkeg/0e9cf64f-ed2a-4e35-d133-e9acf98e7f00/sliderbg';
 
 }
