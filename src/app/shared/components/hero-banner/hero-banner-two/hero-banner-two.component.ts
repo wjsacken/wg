f169afa2b761450f826400eef5b9676fdb54c7c8
wjsacken <wsackenheim@artscube.biz>
2024-04-24 14:56:53 -0400

    test

--- src/app/shared/components/hero-banner/hero-banner-two/hero-banner-two.component.ts
+++ src/app/shared/components/hero-banner/hero-banner-two/hero-banner-two.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-hero-banner-two',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule],
   templateUrl: './hero-banner-two.component.html',
   styleUrl: './hero-banner-two.component.scss'
 })
