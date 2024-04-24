4d296b2a09528fe04f543f44e0822a369c1e9f35
wjsacken <wsackenheim@artscube.biz>
2024-04-24 15:17:12 -0400

    optimizedImage

--- src/app/shared/components/hero-banner/hero-banner-two/hero-banner-two.component.ts
+++ src/app/shared/components/hero-banner/hero-banner-two/hero-banner-two.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common'; // Import NgOptimizedImage
 
 
 @Component({
   selector: 'app-hero-banner-two',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage ],
   templateUrl: './hero-banner-two.component.html',
   styleUrl: './hero-banner-two.component.scss'
 })
 export class HeroBannerTwoComponent {
+  imageUrl: string = 'https://imagedelivery.net/N7RaiyPSCRCLRce1-vDkeg/be5b6665-7d00-4a2c-c1b4-eb4850229300/partners';
 
 }
