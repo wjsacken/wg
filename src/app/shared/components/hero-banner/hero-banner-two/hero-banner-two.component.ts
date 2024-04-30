fe90edb6b97bfff99cafe54b05c420e69854a5ed
wjsacken <wsackenheim@artscube.biz>
2024-04-30 08:53:32 -0400

    Update hero-banner-two.component.ts

--- src/app/shared/components/hero-banner/hero-banner-two/hero-banner-two.component.ts
+++ src/app/shared/components/hero-banner/hero-banner-two/hero-banner-two.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import { NgOptimizedImage } from '@angular/common'; // Import NgOptimizedImage
 
 @Component({
   selector: 'app-hero-banner-two',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule, RouterModule, NgOptimizedImage ],
   templateUrl: './hero-banner-two.component.html',
   styleUrl: './hero-banner-two.component.scss'
 })
